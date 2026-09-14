7e6b7ec2f1af4f78065249d6be55e22f19f6319b
raul-pop-30123 <2005.raul.pop.2005@gmail.com>
2026-09-14 13:50:17 +0300

    Delete MicrocontrollerFirm-MySQL directory

--- MicrocontrollerFirm-MySQL/01_create_database.sql
+++
@@ -1,7 +0,0 @@
--- MicrocontrollerFirm - MySQL 8.0+ / MariaDB 10.2.1+
-
-CREATE DATABASE IF NOT EXISTS MicrocontrollerFirm
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
-
-USE MicrocontrollerFirm;
